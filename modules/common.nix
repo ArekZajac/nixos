d37b9207d087025909b18d14727ce499bb7c8675
Arek Zajac <arekzajac@outlook.com>
2026-04-22 17:40:41 +0100

    Disable SSH password auth

--- modules/common.nix
+++ modules/common.nix
@@ -1,11 +1,12 @@
 { config, pkgs, lib, ... }:
 
 {
+  # System
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
   time.timeZone = "Europe/London";
   i18n.defaultLocale = "en_GB.UTF-8";
 
+  # Packages
   environment.systemPackages = with pkgs; [
     git
     vim
@@ -13,16 +14,23 @@
     wget
   ];
 
+  # Networking
   networking.networkmanager.enable = true;
-
   services.openssh.enable = true;
-  users.mutableUsers = true;
+  services.openssh.settings = {
+    PasswordAuthentication = false;
+    PermitRootLogin = "no";
+  };
 
+  # Users
   users.users.arek = {
     isNormalUser = true;
     description = "arek";
     extraGroups = [ "wheel" "networkmanager" ];
+    openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMSoS4kqlQTPy/OWzzpyVLTTVnoAUQkJ+0QQn0OWPInS"
+      ];
   };
-
+  users.mutableUsers = true;
   security.sudo.wheelNeedsPassword = true;
 }
