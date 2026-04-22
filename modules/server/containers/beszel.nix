54742c2e6cfb45120e3ceea4f7ae282aceca023c
Arek Zajac <arekzajac@outlook.com>
2026-04-22 17:01:46 +0100

    Update beszel.nix

--- modules/server/containers/beszel.nix
+++ modules/server/containers/beszel.nix
@@ -20,6 +20,7 @@
       environment = {
         PORT = "45876";
         KEY = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKX/gj7qUAtqniEMdgFj9yC8JvMFvLHafWQr7lTjmVlV";
+        HUB_URL = "http://localhost:8090";
       };
     };
 
