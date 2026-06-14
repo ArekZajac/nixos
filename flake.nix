3ddbb1e63d9846df69ee75030316e0966fb565db
Arek Zajac <arekzajac@outlook.com>
2026-06-14 13:35:29 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -36,6 +36,7 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "backup";
               home-manager.extraSpecialArgs = { inherit inputs; };
               home-manager.users.arek = import ./home/arek.nix;
             }
